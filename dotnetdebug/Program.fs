9c4071a477bcbd45d96fefbceecf3a76a1c68b2c
Daniel Hauge <animcuil@gmail.com>
2025-03-13 18:11:18 +0100

    some dotnet testing stuff

---
+++ dotnetdebug/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let add x y =
+    let r = x + y
+    r
+
+let result = add 1 2
+
+printfn "Result: %d" result
+
+let args = System.Environment.GetCommandLineArgs()
+printfn "Args: %A" args
+
+let arg1 = args.[1]
