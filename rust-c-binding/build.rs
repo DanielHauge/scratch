93c7b01243582718b8c6d65df5d282b32aee9e94
Daniel Hauge <animcuil@gmail.com>
2025-03-24 20:16:27 +0100

    added rust static linking

--- rust-c-binding/build.rs
+++ rust-c-binding/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("cargo:rustc-link-search=native=.");
     println!("cargo:rustc-link-lib=add");
+    println!("cargo:rustc-link-lib=sub");
 }
--- rust-c-binding/src/main.rs
+++ rust-c-binding/src/main.rs
@@ -3,9 +3,17 @@ extern "C" {
     fn add(a: i32, b: i32) -> i32;
 }
 
+#[link(name = "sub", kind = "static")]
+extern "C" {
+    fn sub(a: i32, b: i32) -> i32;
+}
+
 fn main() {
     let a = 10;
     let b = 20;
     let c = unsafe { add(a, b) };
     println!("{} + {} = {}", a, b, c);
+
+    let c = unsafe { sub(a, b) };
+    println!("{} - {} = {}", a, b, c);
 }
